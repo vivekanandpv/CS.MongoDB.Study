ff9c82ca4d4e078c87a67b0779d3e29259e45cbc
Vivekanand P V <vivekanandpv@outlook.com>
2023-02-05 17:01:32 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CS.MongoDB.Study
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
