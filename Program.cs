816d649043af2801926c957bed392587bc340841
Vivekanand P V <vivekanandpv@outlook.com>
2023-02-05 17:56:42 +0530

    done

--- Program.cs
+++ Program.cs
@@ -1,22 +1,37 @@
-﻿using MongoDB.Driver;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace CS.MongoDB.Study
 {
-    //  Install MongoDB.Driver package
+    //  Create a database called mongo-sample
+    //  In mongo-sample create a collection calle persons
+    //  In persons collection, insert a document through MongoDb Compass
+    //  with: firstName, lastName, city (all strings)
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             MongoClient client = new MongoClient("mongodb://localhost:27017");
 
-            //  List of databases
-            var dbList = client.ListDatabases().ToList();
+            //  Getting the database
+            var mongoSampleDb = client.GetDatabase("mongo-sample");
 
-            //  enumerating
-            foreach (var document in dbList)
+            //  Getting the collection
+            var personsCollection = mongoSampleDb.GetCollection<BsonDocument>("persons");
+
+            //  Getting the cursor
+            var cursor = await personsCollection.FindAsync(p => p["firstName"].AsString.StartsWith("Vivek"));
+
+            //  Getting the first matching document
+            var document = await cursor.FirstAsync();
+
+            Console.WriteLine(document);
+
+            //  Iterating through cursor
+            await cursor.ForEachAsync((d, i) =>
             {
-                Console.WriteLine(document);
-            }
+                Console.WriteLine($"{i} --> {d}");
+            });
         }
     }
 }
