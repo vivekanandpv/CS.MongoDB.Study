317673a7071aa64e9422b5001b6fe7f2fd1ecaa6
Vivekanand P V <vivekanandpv@outlook.com>
2023-02-05 17:38:14 +0530

    done

--- Program.cs
+++ Program.cs
@@ -1,11 +1,22 @@
-﻿namespace CS.MongoDB.Study
+﻿using MongoDB.Driver;
+
+namespace CS.MongoDB.Study
 {
     //  Install MongoDB.Driver package
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+
+            //  List of databases
+            var dbList = client.ListDatabases().ToList();
+
+            //  enumerating
+            foreach (var document in dbList)
+            {
+                Console.WriteLine(document);
+            }
         }
     }
 }
